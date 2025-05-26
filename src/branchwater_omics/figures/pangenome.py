b932292786d9118561904e125b2ad7b359219538
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-05-26 15:00:54 -0700

    Update pangenome.py

--- src/branchwater_omics/figures/pangenome.py
+++ src/branchwater_omics/figures/pangenome.py
@@ -2,6 +2,11 @@
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+
+# ================================== LOCAL IMPORTS =================================== #
+
+from figures import save_figure
 
 # ==================================== FUNCTIONS ===================================== #
 
@@ -20,22 +25,22 @@ def completeness_vs_contamination(df, output_path):
     )
     plt.axvline(90, linestyle='--', color='gray')  # MIMAG completeness cutoff
     plt.axhline(5, linestyle='--', color='gray')   # MIMAG contamination cutoff
-    #plt.show()
+    save_figure(output_path)
 
-def contig_vs_scaffold(df, valid_levels):
+def contig_vs_scaffold(df, valid_levels, output_path):
     sns.boxplot(
-        data=df_clean,
+        data=df,
         x='ncbi_assembly_level',
         y='n50_contigs',
         order=valid_levels  # Ensure order matches filtered data
     )
     plt.yscale('log')
-    #plt.show()
+    save_figure(output_path)
 
-def map(df):
+def map(df, output_path):
     plt.figure(figsize=(10, 6))
     plt.scatter(df['lon'], df['lat'], alpha=0.5)
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Genome Sampling Locations')
-    #plt.show()
+    save_figure(output_path)
